07b3e6e8572d96781b92b84408391442c6803a20
nhannq2 <nhanpublic@gmail.com>
2024-08-10 08:02:09 +0700

    updated sql

--- .class/database/ecommerce.sql
+++ .class/database/ecommerce.sql
@@ -1,76 +1,85 @@
-
 -- users table ---------------------------------------------------
-CREATE TABLE "users" (
-  "user_id" uuid PRIMARY KEY,
-  "name" text NOT NULL,
-  "email" text UNIQUE NOT NULL,
-  "roles" text[] NOT NULL,
-  "password_hash" text NOT NULL,
-  "enabled" boolean NOT NULL,
-  "email_confirm_token" text UNIQUE,
-  "date_created" timestamp NOT NULL,
-  "date_updated" timestamp NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+   user_id               UUID        NOT NULL,
+   name                  TEXT        NOT NULL,
+   email                 TEXT UNIQUE NOT NULL,
+   roles                 TEXT[]      NOT NULL,
+   password_hash         TEXT        NOT NULL,
+   enabled               BOOLEAN     NOT NULL,
+   email_confirm_token   TEXT UNIQUE     NULL,
+   date_created          TIMESTAMP   NOT NULL,
+   date_updated          TIMESTAMP   NOT NULL,
+
+   PRIMARY KEY (user_id)
 );
 
 -- products table ---------------------------------------------------
-CREATE TABLE "products" (
-  "product_id" uuid PRIMARY KEY,
-  "name" text NOT NULL,
-  "desc" text,
-  "imageURL" text,
-  "price" bigint NOT NULL,
-  "quantity" int NOT NULL,
-  "date_created" timestamp NOT NULL,
-  "date_updated" timestamp NOT NULL
+CREATE TABLE IF NOT EXISTS products (
+  product_id    UUID        NOT NULL,
+  name          TEXT        NOT NULL,
+  description   TEXT            NULL,
+  imageURL      TEXT            NULL,
+  price         BIGINT      NOT NULL,
+  quantity      INT         NOT NULL,
+  date_created  TIMESTAMP   NOT NULL,
+  date_updated  TIMESTAMP   NOT NULL,
+
+  PRIMARY KEY (product_id)
 );
 
 -- orders table ---------------------------------------------------
-CREATE TABLE "orders" (
-  "order_id" uuid PRIMARY KEY,
-  "user_id" uuid NOT NULL,
-  "amount" bigint NOT NULL,
-  "status" text NOT NULL,
-  "date_created" timestamp NOT NULL
+CREATE TABLE IF NOT EXISTS orders (
+  order_id      UUID        NOT NULL,
+  user_id       UUID        NOT NULL,
+  amount        BIGINT      NOT NULL,
+  status        TEXT        NOT NULL,
+  date_created  TIMESTAMP   NOT NULL,
+
+  PRIMARY KEY (order_id)
 );
 
-ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+ALTER TABLE orders ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
 
 -- order_items table ---------------------------------------------------
-CREATE TABLE "order_items" (
-  "order_item_id" uuid PRIMARY KEY,
-  "order_id" uuid NOT NULL,
-  "product_id" uuid NOT NULL,
-  "price" bigint NOT NULL,
-  "quantity" int NOT NULL,
-  "date_created" timestamp NOT NULL
+CREATE TABLE IF NOT EXISTS order_items (
+  order_item_id     UUID        NOT NULL,
+  order_id          UUID        NOT NULL,
+  product_id        UUID        NOT NULL,
+  price             BIGINT      NOT NULL,
+  quantity          INT         NOT NULL,
+  date_created      TIMESTAMP   NOT NULL,
+
+  PRIMARY KEY (order_item_id)
 );
 
-CREATE INDEX "product_id_index" ON "order_items" ("product_id");
+CREATE INDEX product_id_index ON order_items (product_id);
 
-CREATE INDEX "order_id_index" ON "order_items" ("order_id");
+CREATE INDEX order_id_index ON order_items (order_id);
 
-ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("order_id");
+ALTER TABLE order_items ADD FOREIGN KEY (order_id) REFERENCES orders (order_id);
 
-ALTER TABLE "order_items" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("product_id");
+ALTER TABLE order_items ADD FOREIGN KEY (product_id) REFERENCES products (product_id);
 
 -- payments table ---------------------------------------------------
-CREATE TABLE "payments" (
-  "payment_id" uuid PRIMARY KEY,
-  "order_id" uuid NOT NULL,
-  "partner" text NOT NULL,
-  "partner_order_id" text NOT NULL,
-  "partner_transaction_id" text UNIQUE,
-  "status" text NOT NULL,
-  "currency" text NOT NULL,
-  "date_created" timestamp NOT NULL
+CREATE TABLE IF NOT EXISTS payments (
+  payment_id                UUID        NOT NULL,
+  order_id                  UUID        NOT NULL,
+  partner                   TEXT        NOT NULL,
+  partner_order_id          TEXT        NOT NULL,
+  partner_transaction_id    TEXT UNIQUE     NULL,
+  status                    TEXT        NOT NULL,
+  currency                  TEXT        NOT NULL,
+  date_created              TIMESTAMP   NOT NULL,
+
+  PRIMARY KEY (payment_id)
 );
 
-CREATE INDEX "order_id_index" ON "payments" ("order_id");
+CREATE INDEX order_id_index ON payments (order_id);
 
-CREATE INDEX "partner_order_id_index" ON "payments" ("partner_order_id");
+CREATE INDEX partner_order_id_index ON payments (partner_order_id);
 
-CREATE INDEX "partner_transaction_id_index" ON "payments" ("partner_transaction_id");
+CREATE INDEX partner_transaction_id_index ON payments (partner_transaction_id);
 
-CREATE INDEX "order_id_status_index" ON "payments" ("order_id", "status");
+CREATE INDEX order_id_status_index ON payments (order_id, status);
 
-ALTER TABLE "payments" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("order_id");
+ALTER TABLE payments ADD FOREIGN KEY (order_id) REFERENCES orders (order_id);
